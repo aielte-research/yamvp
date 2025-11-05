9a57c129353282ed78455c78c3e16c04b57327c5
31b41a59l26u53 <3.1b41a59l26u53@gmail.com>
2025-11-05 11:56:29 +0000

    Update test code

--- test.py
+++ test.py
@@ -69,23 +69,15 @@ if __name__ == "__main__":
     
     # 2-set Area Proportional
     rand2 = np.random.randint(0, 1000, size=(2, 2))
-    fig = venn(rand2, ["Alpha", "Beta"], area_proportional=True)
-    fig.savefig("img/rand2_demo.png", dpi=100, bbox_inches="tight")
-    plt.close(fig)
+    fig = venn(rand2, ["Alpha", "Beta"], area_proportional=True, outfile = "img/rand2_demo.png")
     
     rand2 = np.random.randint(0, 1000, size=(2, 2))
     rand2[1,1] = 0
-    fig = venn(rand2, ["Alpha", "Beta"], area_proportional=True)
-    fig.savefig("img/rand2_demo2.png", dpi=100, bbox_inches="tight")
-    plt.close(fig)
+    fig = venn(rand2, ["Alpha", "Beta"], area_proportional=True, outfile = "img/rand2_demo2.png")
     
     rand2 = np.random.randint(0, 1000, size=(2, 2))
     rand2[1,0] = 0
-    fig = venn(rand2, ["Alpha", "Beta"], area_proportional=True)
-    fig.savefig("img/rand2_demo3.png", dpi=100, bbox_inches="tight")
-    plt.close(fig)
+    fig = venn(rand2, ["Alpha", "Beta"], area_proportional=True, outfile = "img/rand2_demo3.png")
     
     rand2[0,1] = 0
-    fig = venn(rand2, ["Alpha", "Beta"], area_proportional=True)
-    fig.savefig("img/rand2_demo4.png", dpi=100, bbox_inches="tight")
-    plt.close(fig)
+    fig = venn(rand2, ["Alpha", "Beta"], area_proportional=True, outfile = "img/rand2_demo4.png")
